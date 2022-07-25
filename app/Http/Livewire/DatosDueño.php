6f02a64e3319f76e8f67c60d2119cc821fface7e
Celorio2001 <jahir.celorio2001@gmail.com>
2022-07-25 12:11:54 -0500

    Validando la Vista Datos Dueño

--- app/Http/Livewire/DatosDueño.php
+++ app/Http/Livewire/DatosDueño.php
@@ -18,6 +18,7 @@ class DatosDueño extends Component
     }
 
     public function Save(){
+        $this->validate();
         Persona ::create([
             'Cedula' => $this->_Cedula,
             'Nombre' => $this->_Nombre,
@@ -27,5 +28,34 @@ class DatosDueño extends Component
             'id_tipopersona' => $this->_id_tipopersona,
         ]);
         $this->reset();
+        session()->flash('message', 'Guardado Correctamente.');
     }
+
+    public function updated($propertyName)
+    {
+        $this->validateOnly($propertyName);
+    }
+
+    protected $rules = [
+        '_Cedula' => 'required|min:10|max:10',
+        '_Nombre' => 'required|min:3',
+        '_Apellido' => 'required|min:5',
+        '_Telefono' => 'required|min:10|max:10',
+        '_Direccion' => 'required|min:3',
+    ];
+
+    protected $messages = [
+        '_Cedula.required' => 'El Campo es Requerido',
+        '_Cedula.min' => 'El Campo es de 10 Dígitos Minimo',
+        '_Cedula.max' => 'El Campo es de 10 Dígitos Máximo',
+        '_Nombre.required' => 'El Campo es Requerido',
+        '_Nombre.min' => 'El Campo es de 3 Caracteres Minimo',
+        '_Apellido.required' => 'El Campo es Requerido',
+        '_Apellido.min' => 'El Campo es de 5 Caracteres Minimo',
+        '_Telefono.required' => 'El Campo es Requerido',
+        '_Telefono.min' => 'El Campo es de 10 Dígitos Minimo',
+        '_Telefono.max' => 'El Campo es de 10 Dígitos Máximo',
+        '_Direccion.required' => 'El Campo es Requerido',
+        '_Direccion.min' => 'El Campo es de 3 Caracteres Minimo',
+    ];
 }
--- resources/views/livewire/datos-dueño.blade.php
+++ resources/views/livewire/datos-dueño.blade.php
@@ -23,26 +23,31 @@
                       <div class="col-span-6 sm:col-span-6">
                         <label for="cedula_" class="block text-sm font-medium text-gray-700">Cedula</label>
                         <input type="text" name="cedula_" wire:model="_Cedula" id="cedula_" autocomplete="family-name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                        @error('_Cedula') <span class="error">{{ $message }}</span> @enderror
                       </div>
         
                       <div class="col-span-6 sm:col-span-6">
                         <label for="nombre_" class="block text-sm font-medium text-gray-700">Nombre</label>
                         <input type="text" name="nombre_" wire:model="_Nombre" id="nombr_" autocomplete="family-name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                        @error('_Nombre') <span class="error">{{ $message }}</span> @enderror
                       </div>
         
                       <div class="col-span-6 sm:col-span-6">
                         <label for="apellido_ class="block text-sm font-medium text-gray-700">Apellido</label>
                         <input type="text" name="apellido_" wire:model="_Apellido" id="apellido_" autocomplete="family-name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                        @error('_Apellido') <span class="error">{{ $message }}</span> @enderror
                       </div>
     
                       <div class="col-span-6 sm:col-span-6">
                         <label for="telefono_" class="block text-sm font-medium text-gray-700">Télefono</label>
                         <input type="tel" name="telefono_" wire:model="_Telefono" id="telefono_" autocomplete="tel" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                        @error('_Telefono') <span class="error">{{ $message }}</span> @enderror
                       </div>
     
                       <div class="col-span-6 sm:col-span-6">
                         <label for="direccion_" class="block text-sm font-medium text-gray-700">Dirección</label>
                         <input type="text" name="direccion_" wire:model="_Direccion" id="direccion_" autocomplete="address-level4" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                        @error('_Direccion') <span class="error">{{ $message }}</span> @enderror
                       </div>
     
                       <label>Roles</label>
@@ -55,10 +60,18 @@
       
                     </div>
                   </div>
+                  <div>
+                    @if (session()->has('message'))
+                        <div class="alert alert-success" style="width: 250px;border: rgb(72, 136, 53) solid 2px;left: 300px; background: rgb(68, 255, 0)">
+                            {{ session('message') }}
+                        </div>
+                    @endif
+                  </div>
                   <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
                     <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Registrar</button>
                   </div>
                 </div>
+                <br> <br>
               </form>
             </div>
           </div>
